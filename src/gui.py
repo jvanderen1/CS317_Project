9164035071f4cd7851cedf3ca86a47ed7b7bba3c
Joshua Van Deren <livetoscout@gmail.com>
2017-12-02 01:48:27 -0700

    Added marshalling to SQL database to return movie listings

--- src/gui.py
+++ src/gui.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 from src.sqlconnector import SQLConnector
 
+from sortedcontainers import SortedSet
+
 INTERFACE_TITLE = "Search For Available Movies"
 
 ROWS, COLS = 2, 3
@@ -85,7 +87,16 @@ class GUI(tk.Frame):
     def __search_tool(self):
         """Interfaces with MySQL connector to provide a search"""
 
-        print("Entry:", self.search_entry_var.get())
+        # Gathers string from entry box . . .
+        search_term = self.search_entry_var.get()
+
+        # Connects to the database to find all movies playing with search term . . .
+        results = self.sc.find_movies_playing(search_term)
+
+        # Creates pop up dialog box to display movie listings . . .
+        PopupDialog(self.root, 'display_movie_listings', data=results, title="Results for \'%s\'" % search_term)
+
+        # Clears string from entry box . . .
         self.search_entry_var.set("")
 
     # END def __search_tool
@@ -100,7 +111,6 @@ class GUI(tk.Frame):
         self.__search_tool()
 
     # END def __search_tool_entry #
-
     def __quit_program(self):
         """Closes current running tkinter window"""
 
@@ -109,3 +119,79 @@ class GUI(tk.Frame):
         self.destroy()
 
     # END def __quit_program() #
+
+
+class PopupDialog(object):
+    """Dialog used during execution of a tkinter window
+
+    Components:
+        :param: self.top: Figure used on top of a root figure
+    """
+
+    def __init__(self, parent, option, data=None, title=None):
+        """Initializes all necessary variables
+
+        Keyword arguments:
+            :param parent: Tkinter root that will be referenced in this object
+            :param option: Decides which type of popup this dialog is
+            :param data: Data needed for popup (sometimes used)
+            :param title: Title for popup (sometimes used)
+        """
+
+        self.top = tk.Toplevel(parent)
+        self.top.protocol("WM_DELETE_WINDOW", self._on_close)   # Redirects the windows close button to function . . .
+        self.top.resizable(False, False)                        # Prevents windows from being re-sized . . .
+
+        # Finds which option of pop up was chosen . . .
+        if option is 'display_movie_listings':
+            self.top.title(title)
+            self.display_movie_listings(data)
+
+    # END def __init__() #
+
+    def display_movie_listings(self, results):
+        """Displays all available movie listings
+
+        Keyword arguments:
+            :param results: Returned tuples of movies playing with title, date, and time
+        """
+
+        T = tk.Text(self.top)
+        T.pack()
+
+        T.tag_configure('title', font=('Verdana', 25, 'bold'))
+        T.tag_configure('showing', font=('Arial', 15, 'bold', 'italic'))
+
+        # If there are no results . . .
+        if not results:
+            T.insert(tk.END, "Sorry! We couldn't find any movies", 'title')
+
+        # If there are results . . .
+        else:
+
+            # Ordered set, so that it checks for unique showings and displays items in order . . .
+            unique_showings = SortedSet()
+
+            for showing in results:
+                unique_showings.add(showing[0])
+
+            T.insert(tk.END, "There are %d movie(s) showing!\n\n" % len(unique_showings), 'title')
+
+            # Begins displaying both movie titles and show times . . .
+            showtime = 0
+            num_showtimes = len(results)
+            for showing in unique_showings:
+                T.insert(tk.END, showing + '\n\n', 'showing')
+                while showtime < num_showtimes and showing == results[showtime][0]:
+                    T.insert(tk.END, results[showtime][1] + " @ " + results[showtime][2] + '\n')
+                    showtime += 1
+                T.insert(tk.END, '\n')
+
+    # END def display_movie_listings() #
+
+    def _on_close(self):
+        """Closes popup window"""
+
+        self.top.destroy()
+
+    # END def _on_close() #
--- src/sqlconnector.py
+++ src/sqlconnector.py
@@ -2,15 +2,16 @@ import MySQLdb
 from sshtunnel import SSHTunnelForwarder
 
 # ssh variables
-ssh_host = ('prclab.pr.erau.edu', 22)           # Port 22 is default for SSH . . .
+ssh_host = ('prclab.pr.erau.edu', 22)  # Port 22 is default for SSH . . .
 ssh_user = 'vanderj3'
 ssh_pass = 'Atitis00'
 remote_bind_addr = ('pruxap04.erau.edu', 3306)  # Port 3306 is for MySQL . . .
 
 # mysql variables
-mysql_host = '127.0.0.1'                        # 'localhost' will not work for MySQL hostname . . .
+mysql_host = '127.0.0.1'  # 'localhost' will not work for MySQL hostname . . .
 mysql_user = 'vanderenj'
 mysql_pass = 'Atitis00'
+database = 'G3_MovieTheater'
 
 
 class SQLConnector(object):
@@ -40,12 +41,38 @@ class SQLConnector(object):
             host=mysql_host,
             user=mysql_user,
             passwd=mysql_pass,
-            port=self.tunnel.local_bind_port)
+            port=self.tunnel.local_bind_port,
+            database=database
+        )
 
         # Cursor object for writing queries to db . . .
         self.cursor = self.db.cursor()
 
     # END def __init()__ #
+
+    def find_movies_playing(self, search: str) -> str:
+
+        # Surrounded keyword with '%'s for proper SQL execution . . .
+        search = ''.join(['%', search, '%'])
+
+        # SQL command used for finding movie listings . . .
+        cmd = """SELECT m.title, 
+              DATE_FORMAT(s.screening_start, '%%m/%%d/%%y') AS date, 
+              DATE_FORMAT(s.screening_start, '%%h:%%i %%p') AS time 
+              FROM screening AS s 
+              INNER JOIN movie AS m ON m.movie_id = s.movie_id 
+              WHERE m.title LIKE %s 
+              ORDER BY m.title, s.screening_start;"""
+
+        # Executes SQL command (with search parameter) . . .
+        self.cursor.execute(cmd, (search,))
+
+        # Fetches results of SQL execution . . .
+        results = self.cursor.fetchall()
+
+        return results
+
+    # END def find_movies_playing #
 
     def close(self):
         """Closes connection to both database and ssh"""
